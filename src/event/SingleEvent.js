db11978abd9861e864af05044ed875cd3c45380a
Jibbolito <vik.hoffmann1997@gmail.com>
2022-09-26 03:27:15 +0200

    shopping cart fix

--- src/event/SingleEvent.js
+++ src/event/SingleEvent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './SingleEvent.module.css'
 import plus from "./assets/icons8-plus-24.png"
 import cal from "./assets/calendar.png"
@@ -10,35 +10,42 @@ const SingleEvent = ({ event, add, showPlus }) => {
   //adds a saved event
   const handleClick = () => {
     add(event._id);
+    setShow(false);
   }
+
+  const [show, setShow] = useState(true);
 
   let date = new Date(event.date)
 
   return (
-    <div className={styles["container"]}>
-      <div className={styles['pictureFrame']}>
-        <a href={"https://ra.co" + event.contentUrl} target="_blank" >
-          {typeof event.flyerFront !== 'undefined' > 0 ?
-            <img
-              className={styles['picture']}
-              src={event.flyerFront ? event.flyerFront : cal}
-              alt="new"
-            />
-            :
-            <img className={styles['picture']} src={cal} />}
-        </a>
-      </div>
-      <div className={styles['name']}>{event.title}</div>
-      <div className={styles['date']}>| Date: {date.toDateString()}</div>
-      {showPlus && <div className={styles['plus']}>
-        <img src={plus} onClick={handleClick} />
+    <>
+    {show&&
+      <div className={styles["container"]}>
+        <div className={styles['pictureFrame']}>
+          <a href={"https://ra.co" + event.contentUrl} target="_blank" >
+            {typeof event.flyerFront !== 'undefined' > 0 ?
+              <img
+                className={styles['picture']}
+                src={event.flyerFront ? event.flyerFront : cal}
+                alt="new"
+              />
+              :
+              <img className={styles['picture']} src={cal} />}
+          </a>
+        </div>
+        <div className={styles['name']}>{event.title}</div>
+        <div className={styles['date']}>| Date: {date.toDateString()}</div>
+        {showPlus && <div className={styles['plus']}>
+          <img src={plus} onClick={handleClick} />
+        </div>}
+        <div className={styles['plus']}>
+          <a href={event.venue.direction} target="_blank">
+            <img className={styles['location']} src={mapSymbol} />
+          </a>
+        </div>
       </div>}
-      <div className={styles['plus']}>
-        <a href={event.venue.direction} target="_blank">
-          <img className={styles['location']} src={mapSymbol} />
-        </a>
-      </div>
-    </div>
+    </>
+
   )
 }
 
